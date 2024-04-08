9a2b1df940d9e5425a6ae98384e0563dfbb9e27b
ThienLang97 <lampr494@gmail.com>
2024-04-08 17:30:51 +0700

    Header&Footer

---
+++ E-commerce/Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Components/Header'
+import { Footer } from './Components/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+      <div >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In porro, excepturi illo asperiores eius culpa assumenda esse, doloribus iste consectetur tempora sed, maxime perspiciatis nulla optio quibusdam commodi temporibus! Molestiae!
+        Exercitationem corrupti animi harum in numquam ducimus illum, autem ea alias? Ad, facere ut sit officia nostrum quod aperiam dolorem ab in cupiditate perferendis aliquid necessitatibus temporibus excepturi, voluptate nesciunt.
+        Quisquam officiis quibusdam incidunt, suscipit asperiores nam. Praesentium, nemo dolore. Autem, accusantium animi quisquam dolorem ad sapiente aliquam illum officia necessitatibus consequuntur recusandae odio iste delectus ratione temporibus aperiam incidunt.
+        At corrupti natus tempora iusto veritatis excepturi neque illum repellendus similique id ad omnis sit, minus, ipsum, exercitationem vitae reprehenderit delectus. Nobis et, nihil itaque quod fugiat obcaecati necessitatibus adipisci?
+        Saepe, sed? Labore harum nesciunt consequuntur cupiditate fugit. Nam, dolor asperiores! Fugit amet, perferendis alias ea blanditiis facilis eum voluptatem ab a ipsum veritatis nulla harum corrupti culpa voluptates aliquid.
+      </div>
+      <Footer></Footer>
+    </div>
+  )
+}
